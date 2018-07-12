3a7c87f1e131e519469936701a775d04dcf511a6
alex <Xuck.alex@gmail.com>
2018-07-12 22:13:50 +0800

    //todo fix some errors

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,15 +1,63 @@
 import Token._
 import Error._
 import SynTax.SynTaxState._
-class Parser(var syntaxState:SynTaxState,
-             var syntaxLevel:Int,
+
+class Parser(var syntaxState: SynTaxState,
+             var syntaxLevel: Int,
              val lexer: Lexer) {
 
-  def funcBody() = ???
 
-  def parameterTypeList() = ???
+  def printTab(syntaxLevel: Int) = {
+    for(i <- 0 to syntaxLevel)
+      print("\t")
+  }
 
-  def directDeclaratorPostfix():Unit = {
+  def syntaxIndent() = {
+    syntaxState match {
+      case SNTX_NULL =>
+        Lexer.colorToken
+      case SNTX_SP =>
+        print(" ")
+        Lexer.colorToken
+      case SNTX_LF_HT =>
+        if(lexer.token == TK_END)
+          syntaxLevel -= 1
+        print("\n")
+        printTab(syntaxLevel)
+        Lexer.colorToken
+      case SNTX_DELAY =>
+    }
+    syntaxState = SNTX_NULL
+  }
+
+  def parameterTypeList(): Token.Value = {
+    var funcCall: Token.Value = null
+    lexer.getToken()
+    while (lexer.token != TK_CLOSEPA) {
+      if (typeSpecifier() != 0)
+        error("invalid identifier", lexer)
+      declarator()
+      if (lexer.token == TK_CLOSEPA)
+        return funcCall
+      skip(TK_COMMA, lexer)
+      if (lexer.token == TK_ELLIPSIS) {
+        funcCall = KW_CDECL
+        lexer.getToken()
+        return funcCall
+      }
+      funcCall
+    }
+    syntaxState = SNTX_DELAY
+    skip(TK_CLOSEPA, lexer)
+    if (lexer.token == TK_BEGIN)
+      syntaxState = SNTX_LF_HT
+    else
+      syntaxState = SNTX_NULL
+    syntaxIndent()
+    funcCall
+  }
+
+  def directDeclaratorPostfix(): Unit = {
     lexer.token match {
       case TK_OPENPA =>
         parameterTypeList()
@@ -18,9 +66,9 @@ class Parser(var syntaxState:SynTaxState,
         lexer.token match {
           case TK_CINT =>
             lexer.getToken()
-//            n = tkvalue
+          //            n = tkvalue
         }
-        skip(TK_CLOSEBR,lexer)
+        skip(TK_CLOSEBR, lexer)
         directDeclaratorPostfix()
     }
   }
@@ -30,13 +78,13 @@ class Parser(var syntaxState:SynTaxState,
       case TK_IDENT =>
         lexer.getToken()
       case _ =>
-        expect("identifier",lexer)
+        expect("identifier", lexer)
     }
     directDeclaratorPostfix()
   }
 
   def declarator() = {
-    while(lexer.token == TK_STAR)
+    while (lexer.token == TK_STAR)
       lexer.getToken()
     var fc = functionCallingConvention()
     structMemberAlignment
@@ -44,23 +92,153 @@ class Parser(var syntaxState:SynTaxState,
 
   }
 
-  def initializer() = ???
+  def primaryExpression() = {
+    lexer.token match {
+      case TK_CINT =>
+      case TK_CCHAR =>
+        lexer.getToken()
+      case TK_CSTR =>
+        lexer.getToken()
+      case TK_OPENPA =>
+        lexer.getToken()
+        expression()
+        skip(TK_CLOSEPA,lexer)
+      case _ =>
+        val t = lexer.token
+        lexer.getToken()
+        if(t < TK_IDENT)
+          expect("const characters",lexer)
+    }
+  }
+
+  def argumentExpressionList(): Unit = {
+    lexer.getToken()
+    if(lexer.token != TK_CLOSEPA){
+      var flag = true
+      while(flag){
+        assignmentExpression()
+        if(lexer.token == TK_CLOSEPA)
+          flag = false
+        skip(TK_COMMA,lexer)
+      }
+    }
+    skip(TK_CLOSEPA,lexer)
+  }
+
+  def postfixExpression() = {
+    primaryExpression()
+    var flag = true
+    while(flag){
+      lexer.token match {
+        case TK_DOT =>
+          lexer.getToken()
+          lexer.token |= SC_MEMBER
+          lexer.getToken()
+        case TK_POINTSTO =>
+          lexer.getToken()
+          lexer.token |= SC_MEMBER
+          lexer.getToken()
+        case TK_OPENBR =>
+          lexer.getToken()
+          expression()
+          skip(TK_CLOSEBR,lexer)
+        case TK_OPENPA =>
+          argumentExpressionList()
+        case _ =>
+          flag = false
+      }
+    }
+  }
+
+  def sizeofExpression() = {
+    lexer.getToken()
+    skip(TK_OPENPA,lexer)
+    typeSpecifier()
+    skip(TK_CLOSEPA,lexer)
+  }
+
+  def unaryExpression():Unit = {
+    lexer.token match {
+      case TK_AND =>
+        lexer.getToken()
+        unaryExpression()
+      case TK_STAR =>
+        lexer.getToken()
+        unaryExpression()
+      case TK_PLUS =>
+        lexer.getToken()
+        unaryExpression()
+      case TK_MINUS =>
+        lexer.getToken()
+        unaryExpression()
+      case KW_SIZEOF =>
+        lexer.getToken()
+        sizeofExpression()
+      case _ =>
+        postfixExpression()
+    }
+  }
+
+  def multiplicativeExpression() = {
+    unaryExpression()
+    while(lexer.token == TK_STAR || lexer.token == TK_DIVIDE || lexer.token == TK_MOD){
+      lexer.getToken()
+      unaryExpression()
+    }
+  }
+
+  def additiveExpression() = {
+    multiplicativeExpression()
+    while(lexer.token == TK_PLUS || lexer.token == TK_MINUS){
+      lexer.getToken()
+      multiplicativeExpression()
+    }
+  }
+
+  def relationalExpression() = {
+    additiveExpression()
+    while (lexer.token == TK_LT || lexer.token == TK_LEQ
+      || lexer.token == TK_GT || lexer.token == TK_GEQ){
+      lexer.getToken()
+      additiveExpression()
+    }
+  }
+
+  def equalityExpression() = {
+    relationalExpression()
+    while (lexer.token == TK_EQ || lexer.token == TK_NEQ) {
+      lexer.getToken()
+      relationalExpression()
+    }
+  }
+
+  def assignmentExpression():Unit = {
+    equalityExpression()
+    if (lexer.token == TK_ASSIGN) {
+      lexer.getToken()
+      assignmentExpression()
+    }
+  }
+
+  def initializer() = {
+    assignmentExpression()
+  }
 
   def externalDeclaration(l: Int): Unit = {
-    if(typeSpecifier() == 0)
-      expect("<type>",lexer)
+    if (typeSpecifier() == 0)
+      expect("<type>", lexer)
 
     lexer.token match {
       case TK_SEMICOLON =>
         lexer.getToken()
-      case _ => {
+      case _ =>
         var flag = true
-        while(flag){
+        while (flag) {
           declarator()
           lexer.token match {
             case TK_BEGIN => {
-              if(l == SC_LOCAL)
-                error("unsupported internal declaration",lexer)
+              if (l == SC_LOCAL)
+                error("unsupported internal declaration", lexer)
               funcBody()
               flag = false
             }
@@ -71,34 +249,33 @@ class Parser(var syntaxState:SynTaxState,
               lexer.getToken()
             case _ =>
               syntaxState = SNTX_LF_HT
-              skip(TK_SEMICOLON,lexer)
+              skip(TK_SEMICOLON, lexer)
               flag = false
           }
         }
-      }
     }
   }
 
   def structDeclaration() = {
     typeSpecifier()
     var flag = true
-    while(flag){
+    while (flag) {
       declarator()
-      if(lexer.token == TK_SEMICOLON)
+      if (lexer.token == TK_SEMICOLON)
         flag = false
-      skip(TK_COMMA,lexer)
+      skip(TK_COMMA, lexer)
     }
     syntaxState = SNTX_LF_HT
-    skip(TK_SEMICOLON,lexer)
+    skip(TK_SEMICOLON, lexer)
   }
 
   def structDeclarationList() = {
     syntaxState = SNTX_LF_HT
     syntaxLevel += 1
     lexer.getToken()
-    while(lexer.token != TK_END)
+    while (lexer.token != TK_END)
       structDeclaration()
-    skip(TK_END,lexer)
+    skip(TK_END, lexer)
   }
 
   def structSpecifier() = {
@@ -114,10 +291,10 @@ class Parser(var syntaxState:SynTaxState,
       case _ =>
         syntaxState = SNTX_SP
     }
-    syntaxIndet()
-    if(v < TK_IDENT)
-      expect("struct name",lexer)
-    if(lexer.token == TK_BEGIN)
+    syntaxIndent()
+    if (v < TK_IDENT)
+      expect("struct name", lexer)
+    if (lexer.token == TK_BEGIN)
       structDeclarationList()
   }
 
@@ -145,20 +322,20 @@ class Parser(var syntaxState:SynTaxState,
         structSpecifier()
         syntaxState = SNTX_SP
         lexer.getToken()
-      case _ => error("error",lexer,lexer)
+      case _ => error("error", lexer)
     }
     typeFound
   }
 
   def translationUnit() = {
-    while(lexer.token != TK_EOF){
+    while (lexer.token != TK_EOF) {
       externalDeclaration(SC_GLOBAL)
     }
   }
 
-  def functionCallingConvention()= {
+  def functionCallingConvention() = {
     var fc = KW_CDECL
-    if(lexer.token == KW_CDECL || lexer.token == KW_STDCALL){
+    if (lexer.token == KW_CDECL || lexer.token == KW_STDCALL) {
       fc = lexer.token
       syntaxState = SNTX_SP
       lexer.getToken()
@@ -170,15 +347,137 @@ class Parser(var syntaxState:SynTaxState,
     lexer.token match {
       case KW_ALIGN => {
         lexer.getToken()
-        skip(TK_OPENPA)
-        lexer.token  match {
+        skip(TK_OPENPA, lexer)
+        lexer.token match {
           case TK_CINT =>
             lexer.getToken()
           case _ =>
-            expect("need const variable",lexer)
+            expect("need const variable", lexer)
         }
-        skip(TK_CLOSEPA,lexer)
+        skip(TK_CLOSEPA, lexer)
       }
+    }
+  }
+
+  def expression() = {
+    var flag = true
+    while (flag) {
+      assignmentExpression()
+      if (lexer.token != TK_COMMA)
+        flag = false
+      lexer.getToken()
+    }
+  }
+
+  def ifStatement() = {
+    syntaxState = SNTX_SP
+    lexer.getToken()
+    skip(TK_OPENPA, lexer)
+    expression()
+    syntaxState = SNTX_LF_HT
+    skip(TK_CLOSEPA, lexer)
+    statement()
+    if (lexer.token == KW_ELSE) {
+      syntaxState = SNTX_LF_HT
+      lexer.getToken()
+      statement()
+    }
+  }
+
+  def returnStatement() = {
+    syntaxState = SNTX_DELAY
+    lexer.getToken()
+    lexer.token match {
+      case TK_SEMICOLON =>
+        syntaxState = SNTX_NULL
+      case _ => syntaxState = SNTX_SP
+    }
+    syntaxIndent()
+    if (lexer.token != TK_SEMICOLON)
+      expression()
+    syntaxState = SNTX_LF_HT
+    skip(TK_SEMICOLON,lexer)
+  }
+
+  def breakStatement() = {
+    lexer.getToken()
+    syntaxState = SNTX_LF_HT
+    skip(TK_SEMICOLON, lexer)
+  }
+
+  def continueStatement() = {
+    lexer.getToken()
+    syntaxState = SNTX_LF_HT
+    skip(TK_SEMICOLON,lexer)
+  }
+
+  def forStatement() = {
+    lexer.getToken()
+    skip(TK_OPENPA, lexer)
+    if (lexer.token != TK_SEMICOLON)
+      expression()
+    skip(TK_SEMICOLON, lexer)
+    if (lexer.token != TK_SEMICOLON)
+      expression()
+    skip(TK_SEMICOLON,lexer)
+    if (lexer.token != TK_CLOSEPA)
+      expression()
+    syntaxState = SNTX_LF_HT
+    skip(TK_CLOSEPA, lexer)
+    statement()
+  }
+
+  def expressionStatement() = {
+    lexer.token match {
+      case TK_SEMICOLON => expression()
+      case _ =>
+    }
+    syntaxState = SNTX_LF_HT
+    skip(TK_SEMICOLON, lexer)
+  }
+
+  def isTypeSpecifier(token: Token): Boolean = {
+    token match {
+      case KW_CHAR => true
+      case KW_SHORT => true
+      case KW_INT => true
+      case KW_VOID => true
+      case KW_STRUCT => true
+      case _ => false
+    }
+  }
+
+  def compoundStatement() = {
+    syntaxState = SNTX_LF_HT
+    syntaxLevel += 1
+    lexer.getToken()
+    while (isTypeSpecifier(lexer.token))
+      externalDeclaration(SC_LOCAL)
+    while (lexer.token != TK_END)
+      statement()
+    lexer.getToken()
+  }
+
+  def funcBody() = {
+    compoundStatement()
+  }
+
+  def statement() = {
+    lexer.token match {
+      case TK_BEGIN =>
+        compoundStatement()
+      case KW_IF =>
+        ifStatement()
+      case KW_RETURN =>
+        returnStatement()
+      case KW_BREAK =>
+        breakStatement()
+      case KW_CONTINUE =>
+        continueStatement()
+      case KW_FOR =>
+        forStatement()
+      case _ =>
+        expressionStatement()
     }
   }
 }
